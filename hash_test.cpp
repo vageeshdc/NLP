f30e86139b156f897ff7cb7dacb0bfb94607fd5a
Vageesh D C <vageesh@bansi.cse.iitm.ac.in>
2013-08-23 17:24:09 +0530

    completed basic functions

--- hash_test.cpp
+++ hash_test.cpp
@@ -8,26 +8,18 @@
 #include <unordered_map>
 #include <functional>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
+#define MAX_LEVEL 2
+#define MIN_TRESH 1E-6
+
 int hash_func(string word);
 
-struct char_vals{
+double err_arr[4][27][27];
 
-	char letter[26];
-	int counts[26];
-
-};
-
-int del_arr[26][26];
-int swap_arr[26][26];
-int add_arr[26][26];
-int subs_ar[26][26];
-
-int err_arr[4][26][26];
-
-enum ERRORS {SUBS = 0,ADD,DEL,SWAP};
+enum ERRORS {SUBS,ADD,DEL,SWAP};
 
 struct error_pairs{
 	
@@ -36,8 +28,15 @@ struct error_pairs{
 	ERRORS err_type;
 };
 
-unordered_map<string,int> freq_list;
 unordered_map<string,int> word_list;
+
+double var_val;
+
+vector<double> score_list;
+vector<string> name_list;
+
+double check_variants_and_comp(string name,error_pairs epair);
+void generate_variants(string name,int level);
 
 //vector<string> name_list;
 
@@ -65,7 +64,7 @@ void set_word_freq(string name){
 	
 	if(got_elem != word_list.end()){
 		//element is present
-		freq_list[name] ++;
+		word_list[name] ++;
 	}
 
 }
@@ -124,29 +123,189 @@ void update_confusion_mat(string fname){
 	}
 }
 
-double get_score(error_pairs* word_err_list,string name){
+double get_score(error_pairs word_err_list,string name){
 
-	error_pairs* err_ite = word_err_list;
 	double prob = 1.0;
-	int word_count = freq_list[name];
+	int word_count = word_list[name];
 
 	/*
 	 * Lets assume a naive bayes setting
 	 */
-	while(err_ite != 0){
 
-		prob *= 1.0*(err_arr[err_ite->err_type][err_ite->p1][err_ite->p2])/word_count;
+	// incorporate smoothing ...
+	char i,j,k;
+	i = word_err_list.err_type;
+	j = word_err_list.p1;
+	k = word_err_list.p2;
 
-		*err_ite++;
+	if(j == '@'){
+		j = 0;
 	}
+	else{
+		j = j-'a'+1;
+	}
+
+	k -= 'a';
+
+	prob = 1.0*(err_arr[i][j][k])/word_count;
 
 	return prob;
 }
 
 
+void generate_variants_add(string name,int level){
+	
+	/*This is to add a new char*/
+	int i = 0;
+	string tmp_name;
+
+	while(i <= name.length()){
+
+		char j = 'a';
+
+		while(j <= 'z'){
+			tmp_name = name;
+			tmp_name.insert(i,1,j);
+
+			error_pairs elms;
+
+			if(i == 0){
+				elms.p1 = '@';
+			}
+			else{
+				elms.p1 = tmp_name[i-1];
+			}
+
+			elms.p2 = j;
+			elms.err_type = ERRORS::ADD;
+
+			// call the validation function
+			var_val = check_variants_and_comp(tmp_name,elms);
+			if(var_val > -1){
+				name_list.push_back(tmp_name);
+				score_list.push_back(var_val);
+			}
+				
+			if(var_val > MIN_TRESH || var_val < 0)
+				generate_variants(tmp_name,level);
+
+			//increment
+			j++;
+		}
+		i++;
+	}
+}
+
+void generate_variants_subs(string name,int level){
+	
+	/*This is to add a new char*/
+	int i = 0;
+	string tmp_name;
+
+	while(i < name.length()){
+
+		char j = 'a';
+
+		while(j <= 'z'){
+			tmp_name = name;
+			tmp_name[i]= j;
+
+			error_pairs elms;
+			elms.p1 = name[i];
+			elms.p2 = j;
+			elms.err_type = ERRORS::SUBS;
 
 
-void generate_variants(string name){
+			// call the validation function
+			var_val = check_variants_and_comp(tmp_name,elms);
+			if(var_val > -1){
+				name_list.push_back(tmp_name);
+				score_list.push_back(var_val);
+			}
+	
+			if(var_val > MIN_TRESH || var_val < 0)
+				generate_variants(tmp_name,level);
+
+			//increment
+			j++;
+		}
+		i++;
+	}
+}
+
+void generate_variants_del(string name,int level){
+	
+	/*This is to add a new char*/
+	int i = 0;
+	string tmp_name;
+
+	while(i < name.length()){
+
+		tmp_name = name;
+		tmp_name.erase(i,1);
+
+		error_pairs elms;
+
+		if(i == 0){
+			elms.p1 = '@';
+		}
+		else{
+			elms.p1 = tmp_name[i-1];
+		}
+
+		elms.p2 = name[i];
+		elms.err_type = ERRORS::DEL;
+
+		// call the validation function
+		var_val = check_variants_and_comp(tmp_name,elms);
+		if(var_val > -1){
+			name_list.push_back(tmp_name);
+			score_list.push_back(var_val);
+		}
+
+		if(var_val > MIN_TRESH || var_val < 0)
+			generate_variants(tmp_name,level);
+
+		//increment
+		i++;
+	}
+}
+
+void generate_variants_swap(string name,int level){
+	
+	/*This is to add a new char*/
+	int i = 0;
+	string tmp_name;
+
+	while(i < name.length()-1){
+
+		tmp_name = name;
+		char tmp_char = tmp_name[i+1];
+		tmp_name[i+1] = tmp_name[i];
+		tmp_name[i+1] = tmp_char;
+
+		error_pairs elms;
+		elms.p1 = name[i];
+		elms.p2 = name[i+1];
+		elms.err_type = ERRORS::SWAP;
+
+		// call the validation function
+		var_val = check_variants_and_comp(tmp_name,elms);
+		if(var_val > -1){
+			name_list.push_back(tmp_name);
+			score_list.push_back(var_val);
+		}
+		
+		if(var_val > MIN_TRESH || var_val < 0)
+			generate_variants(tmp_name,level);
+		//increment
+		i++;
+	}
+}
+
+
+
+void generate_variants(string name,int level){
 	
 	/*
 	 * THis generates all names
@@ -159,15 +318,29 @@ void generate_variants(string name){
 	 * get the u/p format for words then for training text_corpus and testing the confusion mat
 	 *
 	 */
-	
+	if((level > MAX_LEVEL)||( name_list.size() != 0 )){
+		return;
+	}
 
+	generate_variants_add(name,level+1);
+	generate_variants_subs(name,level+1);
+	generate_variants_del(name,level+1);
+	generate_variants_swap(name,level+1);
+
+	
+	/*
 	//deletion and swap
 	string tmp_name,cpy_name;
 	int i = 0;
 	error_pairs	elem;
+	double var_val;
+
+	vector<double> score_list;
+	vector<string> name_list;
 
 	while(i < name.length()){
 		
+		/// performing deletion
 		tmp_name = name;
 		tmp_name.erase(tmp_name.begin()+i);
 
@@ -182,37 +355,56 @@ void generate_variants(string name){
 
 		elem.err_type = ERRORS::DEL;
 
-		//do something
+		//call to the scoring thing
+		var_val = check_variants_and_comp(tmp_name,elem);
+
+		//pushing the lists
+		if(var_val > -1){
+			name_list.push_back(tmp_name);
+			score_list.push_back(var_val);
+		}
 	
+		/// Performing swaping
 		cpy_name = name;
 		char tmp_char = cpy_name.at(i);
 
 		if(i < name.length()-1){
 			cpy_name[i] = cpy_name[i+1];
 			cpy_name[i+1] = tmp_char;
-			//do something
 			elem.p1 = name[i];
 			elem.p2 = name[i+1];
 			elem.err_type = ERRORS::SWAP;
+
+			//call to scored
+			var_val = check_variants_and_comp(cpy_name,elem);
+
+		//pushing the lists
+			if(var_val > -1){
+				name_list.push_back(tmp_name);
+				score_list.push_back(var_val);
+			}
 		}
 		
 		i++;
 	}
+	*/
 
 }
 
-void check_variants_and_comp(string name,error_pairs epair){
+
+
+double check_variants_and_comp(string name,error_pairs epair){
 
 	//finding the element
 	unordered_map<string,int>::iterator got_elem = word_list.find(name);
 	
 	if(got_elem != word_list.end()){
 
-		
-
+		double score = get_score(epair,name);
+		return score;
 	}
 
-
+	return -1;
 }
 
 int hash_func(string word){
